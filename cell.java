0cbd8d12152cbf6d6fdfc3d8b714c120a63b5eb1
Joachim <jhertaas@gmail.com>
2024-01-29 11:19:30 +0100

    Initial

---
+++ cell.java
@@ -0,0 +1,15 @@
+public class cell {
+    private boolean living;
+
+    public cell(){
+        this.living = false;
+    }
+
+    public boolean isLiving(){
+        return living;
+    }
+
+    public void setLiving(boolean living){
+        this.living = living;
+    }
+}
